d5507dc799405a49f7a7dcb414197dc5aedc4d89
zavadskivadzim <kp002210@mail.ru>
2021-10-23 23:37:10 +0300

    start

--- src/main/java/com/zavadski/task1/Main.java
+++ src/main/java/com/zavadski/task1/Main.java
@@ -4,9 +4,14 @@ package com.zavadski.task1;
 // Если цифр нет, то возвращаемая сумма должна быть равна 0.
 
 public class Main {
-    String string = "мне через 3 месяца и 27 дней будет 30 лет.";
-
-    private int getSumm(){
-        
+    public static void main(String[] args) {
+        String string = "мне через 3 месяца и 27 дней будет 30 лет.";
+        int sum = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isDigit(string.charAt(i))) {
+                sum += Character.getNumericValue(string.charAt(i));
+                System.out.println(sum);
+            }
+        }
     }
 }
